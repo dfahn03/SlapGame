a483c6ae6109f6103acacfa803049bf99d8637ea
Dallin Fahnestock <dfahn03@gmail.com>
2019-05-10 19:13:22 -0600

    Punch 2

--- game.js
+++ game.js
@@ -7,10 +7,28 @@ let target = {
 }
 
 function punch() {
-  target.hits++
+  target.strikes++
   // target.health -= 5
   if (target.strikes > 0) {
     target.health -= 5
+  }
+  updateTarget()
+}
+
+function stab() {
+  target.strickes++
+  // target.health -= 5
+  if (target.strikes > 0) {
+    target.health -= 10
+  }
+  updateTarget()
+}
+
+function doubleStab() {
+  target.strikes++
+  // target.health -= 5
+  if (target.strikes > 0) {
+    target.health -= 10
   }
   updateTarget()
 }
@@ -19,6 +37,7 @@ function updateTarget() {
   document.getElementById('health').innerText = target.health
   document.getElementById('strikes').innerText = target.strikes
 }
+
 
 
 // document.getElementById('hits').innerText = target.health
