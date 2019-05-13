ed8b97c09bce97c1ed588130449e197addd071b0
Dallin Fahnestock <dfahn03@gmail.com>
2019-05-13 00:04:08 -0600

    Reset, Mods Reset, Mods update defenses name

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ let targets = [{
   defenseIndex: 0,
   strikes: 0,
   items: [],
+  itemIndex: 0,
   attacks: {
     punch: 5,
     stab: 10,
@@ -22,26 +23,32 @@ let items = {
 }
 
 function giveParry() {
+  targets[activeTarget].items.shift();
+  targets[activeTarget].itemIndex = 0;
   targets[activeTarget].items.push(items.parry);
-  targets[activeTarget].defenseIndex += 1
+  targets[activeTarget].defenseIndex = 1;
   updateTarget()
 }
 
 function giveSwordBlock() {
+  targets[activeTarget].items.shift();
+  targets[activeTarget].itemIndex = 0;
   targets[activeTarget].items.push(items.swordBlock);
-  targets[activeTarget].defenseIndex += 2
+  targets[activeTarget].defenseIndex = 2;
   updateTarget()
 }
 
 function giveShield() {
+  targets[activeTarget].items.shift();
+  targets[activeTarget].itemIndex = 0;
   targets[activeTarget].items.push(items.shield);
-  targets[activeTarget].defenseIndex += 3
+  targets[activeTarget].defenseIndex = 3;
   updateTarget()
 }
 
 function addMods() {
   let total = 1
-  for (let i = 0; i < targets[activeTarget].items.length; i += 1) {
+  for (let i = 0; i < targets[activeTarget].items.length; i++) {
     let item = targets[activeTarget].items[i];
     total += item
   }
@@ -70,8 +77,12 @@ function updateTarget() {
   document.getElementById('health').innerText = targets[activeTarget].health;
   document.getElementById('strikes').innerText = targets[activeTarget].strikes;
   document.getElementById('defenses').innerText = targets[activeTarget].defenses[targets[activeTarget].defenseIndex]
-  if (targets[activeTarget].health <= targets[activeTarget].death) {
-    document.getElementById('health').innerText = targets[activeTarget].death
+  if (targets[activeTarget].health <= 0) {
+    document.getElementById('health').innerText = 0
+  } else {
+  }
+  if (targets[activeTarget].health >= 100) {
+    document.getElementById('health').innerText = 100
   } else {
   }
   if (targets[activeTarget].health <= targets[activeTarget].death) {
@@ -115,15 +126,18 @@ function reset() {
   targets[activeTarget].health = 100;
   targets[activeTarget].strikes = 0;
   targets[activeTarget].defenseIndex = 0;
+  targets[activeTarget].items.shift();
+  targets[activeTarget].items.shift();
+  targets[activeTarget].items.shift();
+  targets[activeTarget].itemIndex = 0;
   updateTarget()
 }
 
-
-// function setWinMotto() {
-//   if (targets[activeTarget].health <= targets[activeTarget].death) {
-//     document.querySelector('win-motto').innerHTML = `Where other men blindly follow the truth, Remember, nothing is true. Where other men are limited by morality or law, Remember, everything is permitted. We work in the dark to serve the light.`
-//   }
-// }
+function setWinMotto() {
+  if (targets[activeTarget].health <= targets[activeTarget].death) {
+    document.querySelector('.win-motto').innerHTML = `Where other men blindly follow the truth, Remember, nothing is true. Where other men are limited by morality or law, Remember, everything is permitted. We work in the dark to serve the light.`
+  }
+}
 
 // if (targets[activeTarget].health <= targets[activeTarget].death) {
   //   document.getElementById('punch').disabled = true;
